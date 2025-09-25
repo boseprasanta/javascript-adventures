3fc8b034c7a1122c4a56a95097acf05d37d03ffa
Prasanta Bose <pbose1310@gmail.com>
2025-09-25 14:33:47 +0530

    chores: learnt module caching

--- nodeJs-playground/index.js
+++ nodeJs-playground/index.js
@@ -1,6 +1,7 @@
 // like common js - add js is loaded in index.js
 // by this way we do default export
-const add = require("./add.js") // can use any name
+const add = require("./add.js"); // can use any name
+const superHero = require("./super-hero.js");
 
 console.log("hello from node.js", add(1, 6))
 
@@ -12,3 +13,13 @@ var i = 10;
 (function abc() {
    console.log("hi abc") 
 })()
+
+console.log("get super hero name", superHero.getName())
+superHero.setName("Batman")
+console.log("get super hero name batman", superHero.getName())
+
+const superHero2 = require("./super-hero.js")
+
+// should be Spiderman - but will be batman
+// cause in node once the module is required it's cached
+console.log("super hero after redeclare", superHero2.getName())
---
+++ nodeJs-playground/super-hero.js
@@ -0,0 +1,16 @@
+// topic: module caching
+class SuperHero {
+  constructor(name) {
+		this.name = name
+	}
+
+	setName(name) {
+		this.name = name
+	}
+
+	getName() {
+		return this.name
+	}
+}
+
+module.exports = new SuperHero("Spider man")
