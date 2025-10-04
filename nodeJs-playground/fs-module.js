a9808ae98c306013bceda836e2a5dad9edb325c1
Prasanta Bose <pbose1310@gmail.com>
2025-10-04 17:56:17 +0530

    chores: files module refreshed

---
+++ nodeJs-playground/fs-module.js
@@ -0,0 +1,29 @@
+const fs = require("node:fs");
+
+const fileContents = fs.readFileSync("./file.txt", "utf-8"); // returns buffer
+console.log("fileContents", fileContents)
+
+fs.readFile("./file.txt", "utf-8", (error, data) => { // non blocking
+	if (error) {
+		console.log("error", error)
+	} else {
+		console.log("data", data)
+	}
+})
+
+// fs.writeFileSync("./greet.txt", "Hello World!");
+
+fs.writeFile(
+	"./greet.txt", // file path
+	"Hello Vishwa!", // input
+	{ flag: "a" }, // append mode
+	(error) => {
+		if (error) {
+			console.log(error)
+		} else {
+			console.log("File written")
+		}
+});
+
+
+// to read the content of a file
